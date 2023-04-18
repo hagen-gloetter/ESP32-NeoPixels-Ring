2462cfe37ca388f95c2adb1287e402c296b78a1b
Hagen Glötter <github@dg7shg.de>
2023-04-18 23:44:07 +0200

    update brightness

--- code/class_color_wheel.py
+++ code/class_color_wheel.py
@@ -11,7 +11,10 @@ class color_wheel:
         self.pixel_count = pixel_count
         self.pin = pin
         self.np = neopixel.NeoPixel(machine.Pin(self.pin), self.pixel_count)
-        self.brightness=8
+        self.brightness=16
+        self.Rarray = self.pixel_count  * [0]
+        self.Garray = self.pixel_count  * [0]
+        self.Barray = self.pixel_count  * [0]
 
     def show_error(self):
         for i in range(2):
@@ -27,7 +30,42 @@ class color_wheel:
             
             time.sleep(0.5)
 
-    def display_percentage(self, percent):
+    def show_wifi(self):
+        for i in range(2):
+            for j in range(self.np.n):
+                self.np[j] = (0, 0, self.brightness)
+                self.np.write()
+            
+            time.sleep(0.5)
+            
+            for j in range(self.np.n):
+                self.np[j] = (0, 0, 0)
+                self.np.write()
+            
+            time.sleep(0.5)
+            
+    def set_single_color(self, cnt, color):
+        if cnt > self.pixel_count:
+            cnt = self.pixel_count
+        array=self.pixel_count  * [0]
+        for i in range(cnt):
+            array[i] = self.brightness  
+        if "R" == color or "r" == color :
+            self.Rarray=array
+        if "G" == color or "g" == color :
+            self.Garray=array
+        if "B" == color or "b" == color :
+            self.Barray=array
+
+        for i in range(self.pixel_count):
+            time.sleep(0.1)
+            self.np[i] = (self.Rarray[i], self.Garray[i], self.Barray[i])
+            self.np.write()
+        
+        
+        
+
+    def display_percentage1(self, percent):
         if (percent < 0 or percent > 100):
             print("Error: Percentage not correct " + str(percent))
             self.show_error()
@@ -45,7 +83,7 @@ class color_wheel:
                 B = 0
                 G = i*(100/(self.pixel_count-1))
                 R = 100-G
-                print (f"R={R}% G={G}% B={B}%")
+                #print (f"R={R}% G={G}% B={B}%")
                 R = round(R/100*self.brightness)
                 G = round(G/100*self.brightness)
                 self.np[i] = (R, G, B)
@@ -55,6 +93,37 @@ class color_wheel:
                 time.sleep(0.25)
                 self.np[i] = (0, 0, 0)
                 self.np.write()
+
+    def display_percentage2(self, percent):
+        if (percent < 0 or percent > 100):
+            print("Error: Percentage not correct " + str(percent))
+            self.show_error()
+            
+        else:
+            # 12 leds = 100% -> 1 led = 8.3%
+            # tolerance: 12 leds = 96% -> 1 led = 8%
+            #num_leds = round(percent/8.3)
+            num_leds = round(percent/8.3)
+            #print(num_leds)
+            
+            for i in range(num_leds):
+                time.sleep(0.25)
+                #self.np[i] = (round(self.brightness-(i*(100/self.pixel_count))), round(i*(100/self.pixel_count)), 0)
+                B = 0
+                R = i*(100/(self.pixel_count-1))
+                G = 100-R
+                #print (f"R={R}% G={G}% B={B}%")
+                R = round(R/100*self.brightness)
+                G = round(G/100*self.brightness)
+                self.np[i] = (R, G, B)
+                self.np.write()
+            
+            for i in range(self.np.n-1, num_leds-1, -1):
+                time.sleep(0.25)
+                self.np[i] = (0, 0, 0)
+                self.np.write()
+
+
     def show_all(self,cnt=24):
         R = self.brightness
         G = 0 #self.brightness
@@ -66,16 +135,22 @@ class color_wheel:
         
                 
 def main():
-    led_ring = color_wheel(12, 25)
-    led_ring2 = color_wheel(12, 27)
+    led_ring = color_wheel(12, 27)
+    led_ring2 = color_wheel(12, 25)
     
-    led_ring.show_all(12)
-    led_ring.display_percentage(100)
-    led_ring.display_percentage(50)
+    led_ring.set_single_color(12,"r")
+    led_ring.set_single_color(5,"g")
+    led_ring.set_single_color(3,"b")
     
-    led_ring2.show_all(12)
-    led_ring2.display_percentage(100)
-    led_ring2.display_percentage(50)
+#    led_ring.show_wifi()
+#    led_ring.show_all(12)
+#    led_ring.display_percentage1(100)
+#    led_ring.display_percentage1(50)
+    
+#    led_ring2.show_all(12)
+#    led_ring2.show_wifi()
+#    led_ring2.display_percentage2(100)
+#    led_ring2.display_percentage2(50)
     
 
 
--- code/main.py
+++ code/main.py
@@ -6,10 +6,10 @@ import class_wifi_connection
 from class_mqtt import MQTT
 from class_color_wheel import color_wheel
 
-led_ring1 = color_wheel(12, 25)
-led_ring2 = color_wheel(12, 27)
-led_ring1.display_percentage(100)
-led_ring2.display_percentage(100)
+led_ring1 = color_wheel(12, 27)
+led_ring2 = color_wheel(12, 25)
+led_ring1.show_wifi()
+led_ring2.display_percentage2(100)
 
 print ("Setup Wifi")
 global wifi
@@ -19,33 +19,60 @@ wifi = class_wifi_connection.WifiConnect()
 client_id = "led-ring01"
 mqtt = MQTT()
 mqttclient = mqtt.connect(client_id)
-topicSoC1 = b"mqtt.0.Seplos.BatteryPack1.soc"
-topicSoC2 = b"mqtt.0.Seplos.BatteryPack2.soc"
+
 SOC1=0
 SOC2=0
+acoutw=0
 
 def on_message(topic, msg):
     global led_ring1
     global SOC1
     global SOC2
+    global acoutw
     print("Received message `{0}` under topic {1}".format(msg, topic))
-    SOC2 = SOC1
-    SOC1 = float(msg)
-    if SOC2 == 0:
-        SOC2 = SOC1 # fix 1st run
-#    if topic in topicSoC1:
-#        SOC1=int(msg)
-#    if topic in topicSoC2:
-#        SOC2=int(msg)
-    p=(SOC1+SOC2+0.01)/2 # no div/0
-    print (f"SOC1 {SOC1} SOC2 {SOC2} p={p}%")
-    led_ring1.display_percentage(p)
-    led_ring2.display_percentage(p)
+    if "totalsolarw" in topic:
+        p= (int(msg)+1) * 100 / 2500
+        if p < 0:
+            p=0
+        if p > 100:
+            p=100
+        
+        # 12 leds = 100% -> 1 led = 8.3%
+        num_leds = round(p/8.3)
+        led_ring2.set_single_color(num_leds,"g")
+    
+    elif "acoutw" in topic:
+        # gives values from 0 (not poosible) to 4200w (2500w) is normal max therefore 100%
+        p= (int(msg)+1) * 100 / 2500
+        if p < 0:
+            p=0
+        if p > 100:
+            p=100
+        
+        # 12 leds = 100% -> 1 led = 8.3%
+        num_leds = round(p/8.3)
+        led_ring2.set_single_color(num_leds,"r")
+        
+    else:
+        SOC2 = SOC1
+        SOC1 = float(msg)
+        if SOC2 == 0:
+            SOC2 = SOC1 # fix 1st run
+        #    if topic in topicSoC1:
+        #        SOC1=int(msg)
+        #    if topic in topicSoC2:
+        #        SOC2=int(msg)
+        p=(SOC1+SOC2+0.01)/2 # no div/0
+        print (f"SOC1 {SOC1} SOC2 {SOC2} p={p}%")
+        led_ring1.display_percentage1(p)
 
-led_ring1.display_percentage(0)
+led_ring1.display_percentage1(0)
 mqttclient.set_callback(on_message)
-mqttclient.subscribe(topicSoC1)
-mqttclient.subscribe(topicSoC2)
+mqttclient.subscribe(b"mqtt.0.Seplos.BatteryPack1.soc")
+mqttclient.subscribe(b"mqtt.0.Seplos.BatteryPack2.soc")
+mqttclient.subscribe(b"mqtt.0.solaranlage.pip.acoutw")
+mqttclient.subscribe(b"mqtt.0.solaranlage.pip.totalsolarw")
+
 
 test=0
 
@@ -61,13 +88,13 @@ if __name__ == "__main__":
         
     if test==1:
         led_ring1 = color_wheel(12, 10)
-        led_ring1.display_percentage(30)
+        led_ring1.display_percentage1(30)
         time.sleep(2)
-        led_ring1.display_percentage(-10)
+        led_ring1.display_percentage1(-10)
         time.sleep(2)
-        led_ring1.display_percentage(100)
+        led_ring1.display_percentage1(100)
         time.sleep(2)
-        led_ring1.display_percentage(50)
+        led_ring1.display_percentage1(50)
         time.sleep(2)
-        led_ring1.display_percentage(120)
+        led_ring1.display_percentage1(120)
 
